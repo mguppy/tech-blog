bcb982ca57ac1ed6e07473b5a644fb006f9c2708
mguppy <mguppy34@gmail.com>
2021-08-03 16:01:59 -0500

    logging in and out as well as signing up now working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,16 +3,16 @@ const { User, Post } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
-// GET all galleries for homepage
+// GET all posts for homepage
 router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
-      include: [
-        {
-          model: Post,
-          attributes: ['title', 'content'],
-        },
-      ],
+      // include: [
+      //   {
+      //     model: Post,
+      //     attributes: ['title', 'content'],
+      //   },
+      // ],
     });
 
     const posts = dbPostData.map((post) =>
@@ -31,46 +31,46 @@ router.get('/', async (req, res) => {
 
 // GET one gallery
 // Use the custom middleware before allowing the user to access the gallery
-router.get('/gallery/:id', withAuth, async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
   try {
-    const dbGalleryData = await Gallery.findByPk(req.params.id, {
-      include: [
-        {
-          model: Painting,
-          attributes: [
-            'id',
-            'title',
-            'artist',
-            'exhibition_date',
-            'filename',
-            'description',
-          ],
-        },
-      ],
+    const dbPostData = await Post.findByPk(req.params.id, {
+      // include: [
+      //   {
+      //     model: Painting,
+      //     attributes: [
+      //       'id',
+      //       'title',
+      //       'artist',
+      //       'exhibition_date',
+      //       'filename',
+      //       'description',
+      //     ],
+      //   },
+      // ],
     });
 
-    const gallery = dbGalleryData.get({ plain: true });
-    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+    const posts = dbPostData.get({ plain: true });
+    res.render('post', { post, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-// GET one painting
-// Use the custom middleware before allowing the user to access the painting
-router.get('/painting/:id', withAuth, async (req, res) => {
-  try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
+// // GET one painting
+// // Use the custom middleware before allowing the user to access the painting
+// router.get('/painting/:id', withAuth, async (req, res) => {
+//   try {
+//     const dbPaintingData = await Painting.findByPk(req.params.id);
 
-    const painting = dbPaintingData.get({ plain: true });
+//     const painting = dbPaintingData.get({ plain: true });
 
-    res.render('painting', { painting, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
--- seeds/postData.js
+++ seeds/postData.js
@@ -7,6 +7,12 @@ const postData = [
       user_id: 1,
       post_date: 'May 8, 2020',
   },
+  {
+    title: 'Authentication vs. Authorization',
+    content: 'There is a difference between authentication and authorization.  Authentication means confirming your own identity, whereas authorization means being allowed access to the system.',
+    user_id: 1,
+    post_date: 'May 8, 2020',
+},
 ];
 
 const seedPosts = () => Post.bulkCreate(postData);
