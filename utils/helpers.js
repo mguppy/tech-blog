c809b549d18a771b506bcbc0ea8705594dc05b1d
mguppy <mguppy34@gmail.com>
2021-08-11 16:14:36 -0500

    dates formatted correctly

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,7 @@ module.exports = {
   // format_time: (date) => {
   //   return date.toLocaleTimeString();
   // },
-  // format_date: (date) => {
-  //   return date.toLocaleDateString();
-  // },
+  format_date: (date) => {
+    return date.toLocaleDateString();
+  },
 };
