2caf277a79b84db87a601993c5acc4913c75133c
mguppy <mguppy34@gmail.com>
2021-08-12 15:35:30 -0500

    added connection file if statement for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
+// let sequelize;
+
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -9,10 +24,9 @@ const sequelize = new Sequelize(
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-  }
-);
+  },
+)
 
-// let sequelize;
 
 // if (process.env.JAWSDB_URL) {
 //   sequelize = new Sequelize(process.env.JAWSDB_URL);
