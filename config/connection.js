8cb7852280254e402bf4fc76475e9ca4124bb1e7
mguppy <mguppy34@gmail.com>
2021-08-12 16:02:01 -0500

    heroku connection fixed

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
-
-// let sequelize;
-
-const sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -24,8 +9,23 @@ const sequelize = process.env.JAWSDB_URL
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-  },
-)
+  }
+);
+
+// let sequelize;
+
+// const sequelize = process.env.JAWSDB_URL
+// ? new Sequelize(process.env.JAWSDB_URL)
+// : new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   },
+// )
 
 
 // if (process.env.JAWSDB_URL) {
